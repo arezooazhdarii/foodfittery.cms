0115d4f563236b8819584e779577a3a960902f69
arezooazhdari <arezooazhdarii@gmail.co>
2022-11-07 18:52:17 +0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.5"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+	id("com.apollographql.apollo3") version "3.6.2"
+}
+
+group = "com.jambit"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter:latest.release")
+	implementation("com.apollographql.apollo3:apollo-runtime")
+	implementation("com.apollographql.apollo3:apollo-api")
+}
+
+apollo {
+	generateKotlinModels.set(true)
+	packageName.set("com.jambit.foodfittery.cms")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}

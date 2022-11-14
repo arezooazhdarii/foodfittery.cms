2313dd36d9bfa2527f6abe35c77e196ba9c99183
arezooazhdari <arezooazhdarii@gmail.co>
2022-11-14 14:17:28 +0400

    add apollographql

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
+	id("com.apollographql.apollo3").version("3.6.2")
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
 }
@@ -21,7 +22,14 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.apollographql.apollo3:apollo-runtime:3.6.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+apollo {
+	generateKotlinModels.set(true)
+	schemaFile.set(file("src/main/resources/graphql/schema.json"))
+	packageName.set("resources.graphql")
 }
 
 tasks.withType<KotlinCompile> {
